3b544348d75dab6a326cb742c60200b07de0e353
hindlargo <74243541+hindlargo@users.noreply.github.com>
2020-12-09 22:23:25 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+var demenuButton = document.querySelector("header nav button:first-of-type");
+console.log(demenuButton);
+
+function openMenu(){
+    var hetMenu = document.querySelector("header nav ul");
+    hetMenu.classList.toggle("open");
+}
+
+var witHartje = document.querySelector(".like");
+
+function witteHart(){
+    witHartje.innerHTML = "❤️";
+
+}
+
+demenuButton.addEventListener("click", openMenu);
+witHartje.addEventListener("click", witteHart);
