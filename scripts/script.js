9828dec6d28eef9fdb27cdd30e62137da0a883e8
hindlargo <74243541+hindlargo@users.noreply.github.com>
2021-01-14 15:06:05 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
-// JavaScript Document
 var demenuButton = document.querySelector("header nav button:first-of-type");
-console.log(demenuButton);
+var hetMenu = document.querySelector("header nav ul");
+var witHartje = document.querySelector(".like");
+var witHartje1 = document.querySelector(".like1");
+
 
 function openMenu(){
-    var hetMenu = document.querySelector("header nav ul");
     hetMenu.classList.toggle("open");
 }
 
-var witHartje = document.querySelector(".like");
-
 function witteHart(){
     witHartje.innerHTML = "❤️";
+}
 
+function witteHart1(){
+    witHartje1.innerHTML = "❤️";
 }
 
 demenuButton.addEventListener("click", openMenu);
 witHartje.addEventListener("click", witteHart);
+witHartje1.addEventListener("click", witteHart1);
+
